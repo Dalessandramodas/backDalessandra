7820bc68cce95934c3aa78f899260cc46f22dd9f
Vitor Hugo <vitorhugoemail@gmail.com>
2024-06-08 01:32:19 +0000

    tentando resolver erro de cros 3

--- src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
+++ src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
@@ -10,7 +10,10 @@ public class ConfigurandoServidorLocal implements WebMvcConfigurer {
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                      .allowedHeaders("*")
+                      .allowCredentials(true);
+                    ;
                     
         }
     }
