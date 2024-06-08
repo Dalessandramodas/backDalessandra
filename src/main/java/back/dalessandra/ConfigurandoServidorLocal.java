df23f3bc56c0341fba20e3a20c3ff3876800ef73
Vitor Hugo <vitorhugoemail@gmail.com>
2024-06-08 01:09:59 +0000

    tentando resolver erro de cros

--- src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
+++ src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
@@ -9,7 +9,7 @@ public class ConfigurandoServidorLocal implements WebMvcConfigurer {
     @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000","http://backdalessandra-production.up.railway.app")
+                    .allowedOrigins("http://localhost:3000/","http://backdalessandra-production.up.railway.app/"."https://pmv-ads-2024-1-e5-proj-empext-t1-d-alessandra2021.vercel.app/")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         }
     }
