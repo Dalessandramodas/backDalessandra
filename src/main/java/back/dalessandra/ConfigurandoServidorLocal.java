fd999d61b355e0980459d46623760d4968c77fd1
Vitor Hugo <vitorhugoemail@gmail.com>
2024-06-08 00:47:42 +0000

    testefinal

--- src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
+++ src/main/java/back/dalessandra/ConfigurandoServidorLocal.java
@@ -9,7 +9,7 @@ public class ConfigurandoServidorLocal implements WebMvcConfigurer {
     @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://localhost:3000","http://backdalessandra-production.up.railway.app")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         }
     }
